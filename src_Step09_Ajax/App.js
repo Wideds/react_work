4c0b9abea408447b8a46f42a0d00c582754b1609
Wideds <why4517@naver.com>
2023-08-25 11:03:15 +0900

    Step09_Ajax 예제 추가함

---
+++ src_Step09_Ajax/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import PostComponent from './components/PostComponent';
+import PostComponent2 from './components/PostComponent2';
+
+class App extends Component{
+
+  //render() 메소드 에서 리턴해주는 jsx 를 활용해서 화면 구성이 된다.
+  render(){
+    return (
+      <div className="container">
+        <h1>인덱스 페이지 입니다.</h1>
+        <p>어쩌구 저쩌구...</p>
+
+        <PostComponent/>
+        <PostComponent2/>
+      </div>
+    )
+  }
+}
+
+// import 한 곳에 무엇을 리턴해줄지 결정하기
+export default App;
